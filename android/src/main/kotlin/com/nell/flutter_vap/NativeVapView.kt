e3e2f0033a87ff1855ba68d8ae72f0b7db72183b
J Huang <>
2024-10-14 19:38:33 +0800

    dynamic content

--- android/src/main/kotlin/com/nell/flutter_vap/NativeVapView.kt
+++ android/src/main/kotlin/com/nell/flutter_vap/NativeVapView.kt
@@ -2,12 +2,8 @@ package com.nell.flutter_vap
 
 import android.content.Context
 import android.graphics.Bitmap
-import android.graphics.Color
-import android.os.Environment
-import android.util.Log
+import android.graphics.BitmapFactory
 import android.view.View
-import android.widget.TextView
-import kotlinx.coroutines.GlobalScope
 import com.tencent.qgame.animplayer.AnimConfig
 import com.tencent.qgame.animplayer.AnimView
 import com.tencent.qgame.animplayer.inter.IAnimListener
@@ -19,8 +15,11 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.platform.PlatformView
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import java.io.File
+import java.net.URL
+import java.util.Random
 
 
 internal class NativeVapView(binaryMessenger: BinaryMessenger, context: Context, id: Int, creationParams: Map<String?, Any?>?) : MethodChannel.MethodCallHandler, PlatformView {
@@ -61,6 +60,58 @@ internal class NativeVapView(binaryMessenger: BinaryMessenger, context: Context,
             override fun onVideoStart() {
             }
 
+        }
+        )
+
+        vapView.setFetchResource(object: IFetchResource{
+            /**
+             * 获取图片资源
+             * 无论图片是否获取成功都必须回调 result 否则会无限等待资源
+             */
+            override fun fetchImage(resource: Resource, result: (Bitmap?) -> Unit) {
+                /**
+                 * srcTag是素材中的一个标记，在制作素材时定义
+                 * 解析时由业务读取tag决定需要播放的内容是什么
+                 * 比如：一个素材里需要显示多个头像，则需要定义多个不同的tag，表示不同位置，需要显示不同的头像，文字类似
+                 */
+                val srcTag = resource.tag
+                if (srcTag.isNotEmpty()) {
+                    val urlStr = "https://dbekpb58z4pug.cloudfront.net/img/5de243e0-8985-11ef-88a9-2971adb9b100.png?x-oss-process=image/resize,l_600"
+                    val url = URL(urlStr)
+                    val image = BitmapFactory.decodeStream(url.openStream())
+                    result(image)
+
+//                    val drawableId = if (head1Img) R.drawable.head1 else R.drawable.head2
+//                    head1Img = !head1Img
+//                    val options = BitmapFactory.Options()
+//                    options.inScaled = false
+//                    result(BitmapFactory.decodeResource(resources, drawableId, options))
+                } else {
+                    result(null)
+                }
+            }
+
+            /**
+             * 获取文字资源
+             */
+            override fun fetchText(resource: Resource, result: (String?) -> Unit) {
+                val str = "恭喜 No.${1000 + Random().nextInt(8999)}用户 升神"
+                val srcTag = resource.tag
+                if (srcTag.isNotEmpty()) { // 此tag是已经写入到动画配置中的tag
+                    result(str)
+                } else {
+                    result(null)
+                }
+            }
+
+            /**
+             * 播放完毕后的资源回收
+             */
+            override fun releaseResource(resources: List<Resource>) {
+                resources.forEach {
+                    it.bitmap?.recycle()
+                }
+            }
         })
         channel = MethodChannel(binaryMessenger, "flutter_vap_controller")
         channel.setMethodCallHandler(this)
